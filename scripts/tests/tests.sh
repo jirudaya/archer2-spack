b9f41127a664af34067bf5fe5a8668c7f2ccf7ea
lucaparisi91 <luca.parisi91@gmail.com>
2025-10-06 09:39:51 +0100

    Tests only run basic config tests. Installation can be tested by installing the archer2 repo and use reframe to check.

--- scripts/tests/tests.sh
+++ scripts/tests/tests.sh
@@ -11,11 +11,3 @@ echo "spack version found: $(spack --version | grep $VERSION | wc -l)"
 echo "gcc 11 compiler found: $(spack compilers | grep gcc@11 | wc -l)"
 echo "gcc 10 compiler found: $(spack compilers | grep gcc@10 | wc -l)"
 echo "cce 15 compilers found: $(spack compilers | grep cce@15 | wc -l)"
-
-# Check we can build and run a few example packages
-spack -d install -vvv lammps
-spack -d install -vvv quantum-espresso
-
-# Check that we can run a package on the compute nodes
-#sbatch -W submit_lammps.sh
-#cat lammps_test.out
